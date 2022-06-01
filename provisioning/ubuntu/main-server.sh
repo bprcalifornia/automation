5d4c69344f45e28029e877fe3ad02523a8a1932a
Matthew Fritz <mattf@burbankparanormal.com>
2022-05-31 18:33:36 -0700

    Automate the installation of certbot so HTTPS can be enabled

--- provisioning/ubuntu/main-server.sh
+++ provisioning/ubuntu/main-server.sh
@@ -3,11 +3,14 @@
 # main-server.sh
 #
 # Provisioning operations specific to the main Burbank Paranormal Research
-# server
+# server (Ubuntu 22.04 LTS)
 #
 # Ensure the common machine provisioning script (common.sh) is run first
 #
 # sudo chmod +x ./main-server.sh
+#
+# NOTE: superuser commands still use "sudo" so this script can be run under a
+# non-root account even after initial provisioning
 
 # Outputs a line to STDOUT
 #
@@ -15,3 +18,36 @@
 output_line() {
     echo "[PROVISION] $1"
 }
+
+# Installs certbot with Let's Encrypt so we can manage HTTPS certs
+# https://letsencrypt.org/getting-started/
+# https://certbot.eff.org/instructions?ws=nginx&os=ubuntufocal
+#
+# Ex: install_certbot
+install_certbot() {
+    # snap should be pre-installed; if not, see https://snapcraft.io/docs/installing-snap-on-ubuntu
+    
+    # install and refresh snap core
+    sudo snap install core
+    sudo snap refresh core
+
+    # remove any default OS certbot package(s)
+    sudo apt-get remove certbot
+
+    # install certbot itself
+    sudo snap install --classic certbot
+
+    # add a certbot symlink to the user binary directory so it's in PATH by default
+    sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+    # certbot is now installed so we should generate and configure our cert(s)
+    # for Nginx later on in one of two ways:
+    #
+    # 1. Automatic: sudo certbot --nginx
+    # 2. Manual: sudo certbot certonly --nginx
+}
+
+# Install certbot
+output_line "Installing Certbot so we can use Let's Encrypt for HTTPS certs..."
+install_certbot
+output_line "Finished installing Certbot"
